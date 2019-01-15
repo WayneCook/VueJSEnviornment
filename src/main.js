83ae20ba8bc8e2e1ea9930d249bbb68c2cfc09ba
Wayne Cook <WCook@superiorgrocers.com>
2019-01-15 07:09:28 -0800

    Added FA and vue-icon

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
-// import ProfileModal from './components/profileModal.vue';
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import feather from 'vue-icon';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'pretty-checkbox/dist/pretty-checkbox.min.css';
 
+Vue.use(feather, 'v-icon')
 Vue.use(BootstrapVue);
-
 
 new Vue({
   el: '#app',
